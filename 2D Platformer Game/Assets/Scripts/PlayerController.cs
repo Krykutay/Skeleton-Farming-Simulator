13ddc3c89b076c706610a3f5b8eadd63b2699e9c
Kutay Barcin <kutaybarcin@gmail.com>
2021-11-15 14:21:21 +0300

    Character Idle/Walk/Jump animations

--- 2D Platformer Game/Assets/Scripts/PlayerController.cs
+++ 2D Platformer Game/Assets/Scripts/PlayerController.cs
@@ -7,26 +7,51 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float _movementSpeed = 10f;
     [SerializeField] float _jumpForce = 16f;
 
+    [SerializeField] Transform _groundCheck;
+    [SerializeField] float _groundCheckRadius;
+    [SerializeField] LayerMask _groundLayer;
+
+    [SerializeField] int _amountsOfJumps = 2;
+
     Rigidbody2D _rb;
+    Animator _anim;
 
     float _movementInputDirection;
+    int _amountsOfJumpsLeft;
 
     bool _isFacingRight = true;
+    bool _isWalking;
+    bool _isGrounded;
+    bool _canJump;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        _amountsOfJumpsLeft = _amountsOfJumps;
     }
 
     void Update()
     {
         CheckInput();
         CheckMovementDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
     }
 
     void FixedUpdate()
     {
         ApplyMovement();
+        CheckSurroundings();
+    }
+
+    void CheckSurroundings()
+    {
+        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
     }
 
     void CheckMovementDirection()
@@ -35,6 +60,18 @@ public class PlayerController : MonoBehaviour
             Flip();
         else if (!_isFacingRight && _movementInputDirection > 0)
             Flip();
+
+        if (_rb.velocity.x != 0)
+            _isWalking = true;
+        else
+            _isWalking = false;
+    }
+
+    void UpdateAnimations()
+    {
+        _anim.SetBool("isWalking", _isWalking);
+        _anim.SetBool("isGrounded", _isGrounded);
+        _anim.SetFloat("yVelocity", _rb.velocity.y);
     }
 
     void CheckInput()
@@ -47,9 +84,27 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void CheckIfCanJump()
+    {
+        if (_isGrounded && _rb.velocity.y <= 0.1f)
+        {
+            _amountsOfJumpsLeft = _amountsOfJumps;
+        }
+
+        if (_amountsOfJumpsLeft <= 0)
+            _canJump = false;
+        else
+            _canJump = true;
+
+    }
+
     void Jump()
     {
-        _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+        if (_canJump)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+            _amountsOfJumpsLeft--;
+        }
     }
 
     void ApplyMovement()
@@ -63,5 +118,9 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(0f, 180f, 0f);
     }
 
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(_groundCheck.position, _groundCheckRadius);
+    }
 
 }
