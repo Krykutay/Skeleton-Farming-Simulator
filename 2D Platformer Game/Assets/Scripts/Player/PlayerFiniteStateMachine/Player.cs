a22dc700ff7a92fbd54ce381a906744445a36acf
Kutay Barcin <kutaybarcin@gmail.com>
2021-11-25 15:37:02 +0300

    GroundCheck & wallcheck size-length changes

--- 2D Platformer Game/Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
+++ 2D Platformer Game/Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
@@ -250,4 +250,12 @@ public class Player : MonoBehaviour, IDamageable
         stateMachine.currentState.AnimationFinishTrigger();
     }
 
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(_wallCheck.position, new Vector3(_wallCheck.position.x + _playerData.wallCheckDistance, _wallCheck.position.y, _wallCheck.position.z));
+        Gizmos.DrawLine(_ledgeCheck.position, new Vector3(_ledgeCheck.position.x + _playerData.wallCheckDistance, _ledgeCheck.position.y, _ledgeCheck.position.z));
+
+        Gizmos.DrawWireSphere(_ceilingCheck.position, _playerData.groundCheckRadius);
+    }
+
 }
