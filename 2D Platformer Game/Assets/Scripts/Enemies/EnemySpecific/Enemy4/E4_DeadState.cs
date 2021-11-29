bde9c6e9353240c0db2d3c3a8f6c1a22d9381e12
Kutay Barcin <kutaybarcin@gmail.com>
2021-11-29 15:00:02 +0300

    Skeleton anims

--- 2D Platformer Game/Assets/Scripts/Enemies/EnemySpecific/Enemy4/E4_DeadState.cs
+++ 2D Platformer Game/Assets/Scripts/Enemies/EnemySpecific/Enemy4/E4_DeadState.cs
@@ -17,7 +17,7 @@ public class E4_DeadState : DeadState
         base.Enter();
 
         TimeOfDeath = Time.time;
-        Died?.Invoke(enemy);
+        //Died?.Invoke(enemy);
     }
 
     public override void Exit()
@@ -37,6 +37,8 @@ public class E4_DeadState : DeadState
 
             stateMachine.ChangeState(enemy.respawnState);
 
+
+            // TODO: If some other case like it falls from ledge etc, send it to object pool and get it back
             //Enemy4Pool.Instance.ReturnToPool(enemy);
         }
     }
---
+++ 2D Platformer Game/Assets/Scripts/ObjectPoolingScripts/Enemy4Pool.cs
@@ -0,0 +1,4 @@
+public class Enemy4Pool : GenericObjectPool<Enemy4>
+{
+
+}
