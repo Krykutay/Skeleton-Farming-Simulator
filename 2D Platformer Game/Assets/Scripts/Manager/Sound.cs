ad2df63afd4feae79787c64ec93fc954f13cbe5f
Kutay Barcin <kutaybarcin@gmail.com>
2021-12-08 15:20:07 +0300

    SoundManager

---
+++ 2D Platformer Game/Assets/Scripts/Manager/Sound.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[Serializable]
+public class Sound
+{
+    public SoundManager.SoundTags name;
+
+    public SoundManager.SoundTypes type;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume = 1f;
+
+    [Range(.1f, 3f)]
+    public float pitch = 1f;
+
+    [NonSerialized] public float _defaultMaxVolume;
+
+    public bool isLoop;
+    public bool hasCooldown;
+    public AudioSource source;
+}
---
+++ 2D Platformer Game/Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager Instance;
+
+    public Sound[] sounds;
+    private static Dictionary<SoundTags, float> soundTimerDictionary;
+
+    public enum SoundTags
+    {
+        PlayerJump,
+        CoinPickup,
+        PotionPickup,
+        FireballLaunch,
+        FireballHit,
+        Ambient,
+        ButtonClick,
+        Gameover,
+        ScoreCalculation,
+    }
+
+    public enum SoundTypes
+    {
+        Effect,
+        Music,
+    }
+
+    public static SoundManager GetInstance()
+    {
+        return Instance;
+    }
+
+
+    void Awake()
+    {
+        Instance = this;
+
+        soundTimerDictionary = new Dictionary<SoundTags, float>();
+
+        foreach (Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound._defaultMaxVolume = sound.volume;
+
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.isLoop;
+            sound.source.playOnAwake = false;
+
+            if (sound.hasCooldown)
+            {
+                soundTimerDictionary[sound.name] = 0.15f;
+            }
+        }
+    }
+
+    void Start()
+    {
+        //Play(SoundManager.SoundTags.Ambient);
+    }
+
+    public void Play(SoundTags name)
+    {
+        Sound sound = Array.Find(sounds, s => s.name == name);
+
+        if (sound == null)
+        {
+            Debug.LogError("Sound " + name + " Not Found!");
+            return;
+        }
+
+        if (!CanPlaySound(sound)) return;
+
+        sound.source.Play();
+    }
+
+    public void Stop(SoundTags name)
+    {
+        Sound sound = Array.Find(sounds, s => s.name == name);
+
+        if (sound == null)
+        {
+            Debug.LogError("Sound " + name + " Not Found!");
+            return;
+        }
+
+        sound.source.Stop();
+    }
+
+    private static bool CanPlaySound(Sound sound)
+    {
+        if (soundTimerDictionary.ContainsKey(sound.name))
+        {
+            float lastTimePlayed = soundTimerDictionary[sound.name];
+
+            if (lastTimePlayed + (sound.clip.length) / 8 < Time.unscaledTime)
+            {
+                soundTimerDictionary[sound.name] = Time.unscaledTime;
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
+}
