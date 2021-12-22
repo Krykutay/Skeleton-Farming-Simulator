017044187c119305d9365713c3432a9b563d08c1
Kutay Barcin <kutaybarcin@gmail.com>
2021-12-22 20:38:55 +0300

    Powerup System

---
+++ 2D Platformer Game/Assets/Scripts/UI/Powerups/HealthPowerup.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class HealthPowerup : MonoBehaviour
+{
+    public static event Action Collected;
+
+    [SerializeField] float _duration;
+
+    float _spawnTime;
+
+    void OnEnable()
+    {
+        _spawnTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (Time.time >= _spawnTime + _duration)
+        {
+            // Return to the pool
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // play sound
+        Collected?.Invoke();
+        //return to the pool
+    }
+}
