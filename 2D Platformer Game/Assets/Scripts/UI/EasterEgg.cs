a0e1e3cb560185b4ac03ca0b725ce17eb97663ac
Kutay Barcin <kutaybarcin@gmail.com>
2022-01-08 11:24:19 +0300

    Update EasterEgg.cs

--- 2D Platformer Game/Assets/Scripts/UI/EasterEgg.cs
+++ 2D Platformer Game/Assets/Scripts/UI/EasterEgg.cs
@@ -58,7 +58,7 @@ public class EasterEgg : MonoBehaviour
 
     IEnumerator SpawnPowerups()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2.5f);
 
         _easterEggPowerups.SetActive(true);
     }
