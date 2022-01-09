b94f1daaf837dc46b0f00678182643814d69832d
Kutay Barcin <kutaybarcin@gmail.com>
2022-01-09 13:33:51 +0300

    HowToPlay dynamic

--- 2D Platformer Game/Assets/Scripts/UI/Menu/HowToPlayPanelControlsDisplay.cs
+++ 2D Platformer Game/Assets/Scripts/UI/Menu/HowToPlayPanelControlsDisplay.cs
@@ -3,15 +3,24 @@ using TMPro;
 
 public class HowToPlayPanelControlsDisplay : MonoBehaviour
 {
-    [SerializeField] TMP_Text _currentJumpKeybindText;
-    [SerializeField] TMP_Text _currentFireKeybindText;
-
-    [SerializeField] TMP_Text _displayedJumpKeybindText;
-    [SerializeField] TMP_Text _displayedFireKeybindText;
+    [SerializeField] TMP_Text _displayedMoveLeftRightKeybindText;
+    [SerializeField] TMP_Text _displayedJumpbindText;
+    [SerializeField] TMP_Text _displayedAttackKeybindText;
+    [SerializeField] TMP_Text _displayedParryKeybindText;
+    [SerializeField] TMP_Text _displayedDashKeybindText;
+    [SerializeField] TMP_Text _displayedCrouchKeybindText;
+    [SerializeField] TMP_Text _displayedInteractKeybindText;
 
     void OnEnable()
     {
-        _displayedJumpKeybindText.text = "Jump/Fly: [" + _currentJumpKeybindText.text + "]";
-        _displayedFireKeybindText.text = "Fire: [" + _currentFireKeybindText.text + "]";
+        _displayedMoveLeftRightKeybindText.text = "Left: [" + GameAssets.Instance.keybinds[(int)GameAssets.Keybinds.MoveLeft].text + "] -" +
+            " Right: [" + GameAssets.Instance.keybinds[(int)GameAssets.Keybinds.MoveLeft].text + "]";
+
+        _displayedJumpbindText.text = "Jump: ["+ GameAssets.Instance.keybinds[(int)GameAssets.Keybinds.Jump].text + "]";
+        _displayedAttackKeybindText.text = "Attack: [" + GameAssets.Instance.keybinds[(int)GameAssets.Keybinds.Attack].text + "]";
+        _displayedParryKeybindText.text = "Parry: [" + GameAssets.Instance.keybinds[(int)GameAssets.Keybinds.Parry].text + "]";
+        _displayedDashKeybindText.text = "Dash: [" + GameAssets.Instance.keybinds[(int)GameAssets.Keybinds.Dash].text + "]";
+        _displayedCrouchKeybindText.text = "Crouch: [" + GameAssets.Instance.keybinds[(int)GameAssets.Keybinds.Crouch].text + "]";
+        _displayedInteractKeybindText.text = "Interact: [" + GameAssets.Instance.keybinds[(int)GameAssets.Keybinds.Interact].text + "]";
     }
 }
