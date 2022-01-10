3e7153abe15828cf2d7dc450a0fc04cd1d8edb88
Kutay Barcin <kutaybarcin@gmail.com>
2022-01-10 11:56:04 +0300

    Update SurfaceHit.cs

--- 2D Platformer Game/Assets/Scripts/SceneObjects/SurfaceHit.cs
+++ 2D Platformer Game/Assets/Scripts/SceneObjects/SurfaceHit.cs
@@ -6,7 +6,7 @@ public class SurfaceHit : MonoBehaviour
 {
     BoxCollider2D _collider;
     List<Collider2D> collisions;
-    [SerializeField] LayerMask _playerAndEnemy;
+    LayerMask _mask;
 
     Coroutine _hitCollisions = null;
 
@@ -14,6 +14,8 @@ public class SurfaceHit : MonoBehaviour
     {
         _collider = GetComponent<BoxCollider2D>();
         collisions = new List<Collider2D>();
+
+        _mask = LayerMask.GetMask("Player", "Damageable");
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -36,8 +38,8 @@ public class SurfaceHit : MonoBehaviour
     {
         while (collisions.Count > 0)
         {
-            Collider2D[] collision = Physics2D.OverlapBoxAll(_collider.bounds.center, _collider.bounds.size, 0f, _playerAndEnemy);
-            foreach (var col in collision)
+            Collider2D[] collision = Physics2D.OverlapBoxAll(_collider.bounds.center, _collider.bounds.size, 0f, _mask);
+            foreach (Collider2D col in collision)
             {
                 if (col.TryGetComponent<Entity>(out Entity entity))
                     entity.DamageBySurface();
